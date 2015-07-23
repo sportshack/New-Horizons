0167047114c1b464f4e863ebf2c42f054223af72
sberdine77 <sbsx77xavier@hotmail.com>
2015-07-23 11:41:15 -0300

    Initial Commit

---
+++ LocationTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocationTest
+//
+//  Created by Sávio Xavier on 7/23/15.
+//  Copyright (c) 2015 Sávio Xavier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocationTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocationTest
+//
+//  Created by Sávio Xavier on 7/23/15.
+//  Copyright (c) 2015 Sávio Xavier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
